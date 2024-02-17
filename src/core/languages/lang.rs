2f2e5e74573eeed74ee65ae9d9c99d6f76a892cf
Braden Everson <bradeneverson@gmail.com>
2024-02-17 09:32:20 -0600

    Add french and italian support

--- src/core/languages/lang.rs
+++ src/core/languages/lang.rs
@@ -1,11 +1,15 @@
 pub enum Language {
     English,
+    French,
+    Italian
 }
 
 impl Language {
     pub fn get_stem(&self) -> &str {
         return match self {
-            Language::English => "en"
+            Language::English => "en",
+            Language::French => "fr",
+            Language::Italian => "it"
         };
     }
 }
